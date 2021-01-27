c76abedccae16e5dadc0f702ea49cc2b7a5f1455
Sandy Pounoussamy <pounoussamy.sandy@gmail.com>
2021-01-27 16:51:17 +0000

    Refactored interactive menu into separated methods

--- directory.rb
+++ directory.rb
@@ -1,35 +1,76 @@
+@students = []
+
 def input_student
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
+  months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"]
 
   name = gets.chomp
+  months.each_with_index { |month, index| puts "#{index + 1}. #{month}" }
+  month_number = gets.strip
+  cohort = months[month_number.to_i - 1]
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
+    if !name.empty?
+      puts "Enter cohort month number"
+      month_number = gets.chomp
+      cohort = months[month_number.to_i - 1]
+    end
   end
-  students
 end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
-  students.each do |student|
+
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_student
-if students.count > 0
-  print_header
-  print(students)
-  print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
+
+def show_student
+  if @students.count > 0
+    print_header
+    print_student_list
+    print_footer
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_student
+    when "2"
+      show_student
+    when "9"
+      exit # exit program
+    else
+      puts "I didn't get that. Please try again."
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
